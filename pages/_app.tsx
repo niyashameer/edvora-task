7e13401aa24ce1b41ee987e645e9067c3c11f6ba
niyashameer <sniya359@gmail.com>
2022-02-05 23:56:15 +0530

    🐛 Font on vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Head from "next/head";
+import path from 'path';
+
+path.resolve(process.cwd(), 'fonts', 'fonts.conf');
+path.resolve(process.cwd(), 'fonts', 'SF-Pro-Rounded-Medium.otf');
+path.resolve(process.cwd(), 'fonts', 'SFPRODISPLAYREGULAR.OTF');
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (

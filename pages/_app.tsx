6081bfc269c8491b7b76807aa5298ede3334aea5
niyashameer <sniya359@gmail.com>
2022-02-05 22:20:05 +0530

    🐛 Font fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,27 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<div className="bg-background text-white tracking-wider">
-			<Component {...pageProps} />
+		<div className='bg-background text-white tracking-wider'>
+			<Head>
+				<link
+					rel='preload'
+					href='/assets/fonts/SFPRODISPLAYREGULAR.otf'
+					as='font'
+					crossOrigin=''
+				/>
+				<link
+					rel='preload'
+					href='/assets/fonts/SF-Pro-Rounded-Medium.otf'
+					as='font'
+					crossOrigin=''
+				/>
+			</Head>
+			<body>
+				<Component {...pageProps} />
+			</body>
 		</div>
 	);
 }
